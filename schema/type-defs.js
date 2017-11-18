03e4ce84c51383b7fb7bda23456bdf0ed3a84b0d
Vampiire <vamp@Drs-MacBook-Pro.home>
2017-11-18 03:20:41 -0500

    updated GQL schemas
    minor changes to the City and Cohort models (spacing)

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,33 +1,73 @@
 module.exports = `
-  enum _userStatus {
+  scalar Date
+
+  enum _user_status {
     pending_approval
     profile_incomplete
     profile_complete
   }
 
+  enum _cohort_status {
+    registration_open
+    registration_closed
+    users_accepted
+    tiers_assigned
+    teams_assigned
+    ongoing
+    ended
+  }
+
+  enum _cohort_team_user_role {
+    project_manager
+    member
+  }
+
   type Country {
-    title: String!
-    description: String
+    name: String!
+    users: [User!]!
+    city: City
+    group: Group!
   }
 
   type City {
-    title: String!
-    description: String
+    name: String!
+    group: Group!
+    country: Country!
+    users: [User!]!
   }
 
   type Cohort {
     title: String!
-    description: String
-  } 
+    status: _cohort_status!
+    start_date: Date
+    end_date: Date
+    users: [User!]!
+    teams: [CohortTeam!]!
+    group: Group!
+  }
+  
+  type CohortUser {
+    placeholder: String!
+  }
 
   type CohortTeam {
     title: String!
-    description: String
+    tier: _cohort_team_tier!
+    cohort: Cohort!
+    project: Project!
+    users: [CohortTeamUser!]!
+  }
+
+  type CohortTeamUser {
+    role: _cohort_team_user_role
+    user: User!
+    cohort: Cohort!
   }
 
   type Group {
     title: String!
-    description: String
+    owner: String!
+    users: [User!]!
   }
 
   type Project {
@@ -35,26 +75,27 @@ module.exports = `
     description: String
     project_url: String
     github_url: String
+    users: [User!]!
   }
 
   type User {
     email: String!
     username: String!
-    firstName: String!
-    lastName: String!
-    status: _userStatus!
-    githubURL: String!
-    linkedinURL: String
-    portfolioURL: String
-    websiteURL: String
-    twitterURL: String
-    blogURL: String
+    first_name: String!
+    last_name: String!
+    status: _user_status!
+    github_url: String!
+    linkedin_url: String
+    portfolio_url: String
+    website_url: String
+    twitter_url: String
+    blog_url: String
     country: Country!
     city: City
-    profileImage: String
+    profile_image: String
     projects: [Project!]!
     cohorts: [Cohort!]!
-    cohortTeams: [CohortTeam!]!
+    cohort_teams: [CohortTeam!]!
     groups: [Group!]!
   }
 
