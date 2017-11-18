180c854bb9c6c0276d56ed48cb640b3758bd1fc9
Vampiire <vamp@new-host-4.home>
2017-11-18 15:57:23 -0500

    create City query resolver

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,5 +1,9 @@
 module.exports = {
-  Query: {},
+  Query: {
+    city: async (root, { city_id }, { models: { City } }) => {
+      return await city.findById(city_id)
+    }, 
+  },
 
   Mutation: {}
 };
