4640bef85fd21ce859a700615c7a0869cd3e528a
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-19 00:23:37 +0200

    Add projects resolver

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,25 +1,28 @@
 module.exports = {
   Query: {
     user: async (root, { username, user_id }, { models: { User } }) => {
-      return await username ? User.findOne({ where: { username } }) : User.findById(user_id)
+      return (await username)
+        ? User.findOne({ where: { username } })
+        : User.findById(user_id);
     },
     city: async (root, { city_id }, { models: { City } }) => {
       return await City.findById(city_id);
-    }, 
+    },
     country: async (root, { country_id }, { models: { Country } }) => {
       return await Country.findById(country_id);
     },
     group: async (root, { group_id }, { models: { Group } }) => {
       return await Group.findById(group_id);
+    },
+    projects: async (root, { limit, offset }, { models: { Project } }) => {
+      return await Project.findAll({ limit, offset });
     }
   },
 
-  Mutation: {
-
-  },
+  Mutation: {},
 
   User: {
     // custom property resolvers
-      // format -> property: async...
+    // format -> property: async...
   }
 };
