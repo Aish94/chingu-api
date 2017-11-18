861606a505fc8058409f70982f55e1c42d54e228
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-18 12:37:25 +0200

    Create cohort_user model & migration

---
+++ migrations/20171118103050-create-cohort-user.js
@@ -0,0 +1,62 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('cohort_users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      cohort_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'cohorts',
+          key: 'id'
+        }
+      },
+
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM,
+        values: [
+          'pending_approval',
+          'rejected',
+          'accepted',
+          'tier_assigned',
+          'team_assigned'
+        ],
+        defaultValue: 'pending_approval'
+      },
+      tier: {
+        type: Sequelize.ENUM,
+        allowNull: false,
+        values: [1, 2, 3, 4]
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('cohort_users');
+  }
+};
