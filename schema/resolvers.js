912a99681ec7d06c4916ef69f40b2b8050d1a38b
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-19 23:38:22 +0200

    Fix linter issues

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -6,21 +6,22 @@ const requireAdmin = (user) => {
 
 module.exports = {
   Query: {
-    user: async (root, { username, user_id }, { models: { User } }) => username
-        ? await User.findOne({ where: { username } })
-        : await User.findById(user_id),
+    user: async (root, { username, user_id }, { models: { User } }) => {
+      if (username) await User.findOne({ where: { username } });
+      else await User.findById(user_id);
+    },
 
-    city: async (root, { city_id }, { models: { City } }) => await City.findById(city_id),
+    city: async (root, { city_id }, { models: { City } }) => City.findById(city_id),
 
-    country: async (root, { country_id }, { models: { Country } }) => await Country.findById(country_id),
+    country: async (root, { country_id }, { models: { Country } }) => Country.findById(country_id),
 
-    group: async (root, { group_id }, { models: { Group } }) => await Group.findById(group_id),
+    group: async (root, { group_id }, { models: { Group } }) => Group.findById(group_id),
 
-    cohort: async (root, { cohort_id }, { models: { Cohort } }) => await Cohort.findById(cohort_id),
+    cohort: async (root, { cohort_id }, { models: { Cohort } }) => Cohort.findById(cohort_id),
 
-    cohorts: async (root, { limit, offset }, { models: { Cohort } }) => await Cohort.findAll({ limit, offset }),
+    cohorts: async (root, { limit, offset }, { models: { Cohort } }) => Cohort.findAll({ limit, offset }),
 
-    projects: async (root, { limit, offset }, { models: { Project } }) => await Project.findAll({ limit, offset }),
+    projects: async (root, { limit, offset }, { models: { Project } }) => Project.findAll({ limit, offset }),
   },
 
   Mutation: {
@@ -29,7 +30,7 @@ module.exports = {
       // create the Country Group
       const group = await Group.create({ title: `${name} Group`, group_type: 'Country' });
       // create Country
-      return await Country.create({ name, group_id: group.id });
+      await Country.create({ name, group_id: group.id });
     },
 
     createCohort: async (root, { title }, { models: { Cohort, Group }, user }) => {
@@ -37,18 +38,18 @@ module.exports = {
       // create the Cohort Group
       const group = await Group.create({ title: `${title} Group`, group_type: 'Cohort' });
       // create Cohort
-      return await Cohort.create({ title, group_id: group.id });
+      await Cohort.create({ title, group_id: group.id });
     },
 
-    createCity: async (root, { country_id, name }, { models: { City, Group }, user }) => {
+    createCity: async (root, { country_id, name }, { models: { City }, user }) => {
       requireAdmin(user);
-      return await City.create({ country_id, name });
+      await City.create({ country_id, name });
     },
 
     changeUserStatus: async (root, { user_id, status }, { models: { User }, user }) => {
       requireAdmin(user);
       const target_user = await User.findById(user_id);
-      return await target_user.update({ status });
+      await target_user.update({ status });
     },
 
     signInUser: async (root, { email, password }, { models: { User } }) => {
@@ -71,11 +72,11 @@ module.exports = {
     },
 
     updateUser: async (root, { user_data }, { user }) => {
-      user.email = undefined;
-      if (user.status === 'pending_approval') {
-        user_data.username = undefined;
-      }
-      return await user.update(user_data);
+      const updated_user = Object.assign({}, user_data, {
+        username: user.status === 'pending_approval' ? undefined : user_data.username,
+        email: undefined,
+      });
+      await user.update(updated_user);
     },
 
     createCohortTeam: async (root, data, { models: { CohortTeam }, user }) => {
