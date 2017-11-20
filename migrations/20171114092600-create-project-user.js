4e28a105fbe736a6983f21f4f2b0711bcb49149c
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-20 17:32:49 +0200

    LINT LINT LINT

--- migrations/20171114092600-create-project-user.js
+++ migrations/20171114092600-create-project-user.js
@@ -1,50 +1,45 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('project_users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
+  up: (queryInterface, Sequelize) => queryInterface.createTable('project_users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
 
-      user_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'users',
-          key: 'id'
-        }
+    user_id: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'users',
+        key: 'id',
       },
-      project_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'projects',
-          key: 'id'
-        }
+    },
+    project_id: {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'projects',
+        key: 'id',
       },
+    },
 
-      role: {
-        allowNull: false,
-        type: Sequelize.ENUM,
-        values: ['project_manager', 'collaborator']
-      },
+    role: {
+      allowNull: false,
+      type: Sequelize.ENUM,
+      values: ['project_manager', 'collaborator'],
+    },
 
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('project_users');
-  }
+    created_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('project_users'),
 };
