a23a0fc5650cfe8d26a747e995079e63874dde7f
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-20 17:44:17 +0200

    Migration changes

--- migrations/20171120150129-project-description-allow-null.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.changeColumn('projects', 'description', { allowNull: true, type: Sequelize.TEXT }),
-  down: (queryInterface, Sequelize) => queryInterface.changeColumn('projects', 'description', { allowNull: false, type: Sequelize.TEXT }),
-};
