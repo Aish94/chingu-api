0ab27e14670952e5e9d8bf518c59c8e0406c804e
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-20 04:54:38 +0200

    Add bio to user model

--- config/utilities.js
+++ config/utilities.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports.loadConfigFile = (file_name) => {
-  if (fs.existsSync(`${__dirname}/../config/${file_name}-local.js`)) {
-    return require(`${__dirname}/../config/${file_name}-local.js`);
-  } else if (fs.existsSync(`${__dirname}/../config/${file_name}.js`)) {
-    return require(`${__dirname}/../config/${file_name}.js`);
+const getConfigPath = (file_name) => {
+  if (fs.existsSync(path.resolve('config', `${file_name}-local.js`))) {
+    return path.resolve('config', `${file_name}-local.js`);
+  } else if (fs.existsSync(path.resolve('config', `${file_name}.js`))) {
+    return path.resolve('config', `${file_name}.js`);
   }
   throw new Error(`File ${file_name} is not a config file that exists.`);
 };
+
+const loadConfigFile = file_name => require(getConfigPath(file_name));
+
+module.exports = { getConfigPath, loadConfigFile };
