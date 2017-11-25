30ea8094792cc7b3b36ac7ee8532330f6b191ff2
Musaab Abdalla <musabelfaki@gmail.com>
2017-11-25 23:31:54 +0200

    default url param

--- config/database.js
+++ config/database.js
@@ -22,7 +22,7 @@ module.exports = {
     },
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     define: {
       underscored: true,
