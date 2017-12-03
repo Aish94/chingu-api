4831ec1984e728f73baeb50ea432b9872e062d06
Musaab Abdalla <musabelfaki@gmail.com>
2017-12-03 03:21:43 +0200

    Renamed env variable

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   MONGO_URL: process.env.MONGO_URL,
-  AUTOBOT_CDN_API_SECRET: process.env.AUTOBOT_SECRET,
+  AUTOBOT_CDN_API_SECRET: process.env.AUTOBOT_CDN_API_SECRET,
 };
