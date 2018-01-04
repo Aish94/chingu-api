68866a2b11bcc23a3eb7cb622fb8d288a798ad67
Jay Schwane <jrschwane@uwalumni.com>
2018-01-04 00:15:02 -0600

    Additional work on metadata & metadata_schema models

--- models/metadata.js
+++ models/metadata.js
@@ -1,7 +1,64 @@
 module.exports = (sequelize, DataTypes) => {
   const Metadata = sequelize.define('Metadata', {
-    metadata_schema_id: DataTypes.INTEGER,
+
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+
+    metadata_schema_id: {
+      allowNull: true,
+      type: DataTypes.INTEGER,
+      onDelete: 'SET NULL',
+      references: {
+        model: 'metadata_schemas',
+        key: 'id',
+      },
+    },
+
+    metadata: {
+      allowNull: false,
+      type: DataTypes.JSONB,
+    },
+
+    metadata_source: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        'slack',
+        'github',
+      ],
+    },
+
+    entity_type: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        'CohortUser',
+        'CohortChannel',
+        'CohortChannelUser',
+        'CohortTeam',
+      ],
+    },
+
+    entity_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+
   });
+
+  Metadata.associate = (models) => {
+    Metadata.belongsTo(models.MetadataSchema);
+    // one to many?
+    Metadata.hasMany(models.CohortUser);
+    Metadata.hasMany(models.CohortTeam);
+    // Metadata.hasMany(models.CohortChannelUser);
+    // Metadata.hasMany(models.CohortChannel);
+    // Metadata.hasMany(models.Project);
+  };
 
   return Metadata;
 };
--- models/metadata_schema.js
+++ models/metadata_schema.js
@@ -1,14 +1,48 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var metadata_schema = sequelize.define('metadata_schema', {
-    metadata_schema_id: DataTypes.INTEGER
-  }, {
-    underscored: true,
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+  const MetadataSchema = sequelize.define('MetadataSchema', {
+
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+
+    metadata_source: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        'slack',
+        'github',
+      ],
+    },
+
+    entity_type: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: [
+        'CohortUser',
+        'CohortChannel',
+        'CohortChannelUser',
+        'CohortTeam',
+      ],
+    },
+
+    schema_version: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+
+    metadata_structure: {
+      allowNull: false,
+      type: DataTypes.JSONB,
+    },
+
   });
-  return metadata_schema;
+
+  MetadataSchema.associate = (models) => {
+    MetadataSchema.hasMany(models.Metadata);
+  };
+
+  return MetadataSchema;
 };
