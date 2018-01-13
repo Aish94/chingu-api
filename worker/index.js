542a0e7980b9df024bf560347a3ce43eaf288c19
The-Vampiire <vampiirecodes@gmail.com>
2018-01-12 20:35:41 -0500

    added slackmetrics tool
    installed SlackMetrics tool
    
    configured the SMT in the cohort scrape task
    
    tested and verified to be working
    
    FUCK PACKAGE-LOCK

--- worker/index.js
+++ worker/index.js
@@ -1,10 +1,36 @@
+// DO NOT TOUCH MY LOGGY BOI
 console.log('Worker running - loggy boi');
-const { Wizard, CohortChannel } = require('../models'); 
-const { ScrapeQ: { queue, tasks: { cohort_scrape } } } = require('../queues/index');
+// DO NOT TOUCH - PRODUCTION READY
+// WE DONT KNOW HOW HE WORKS BUT HE DOES
+const { Wizard, CohortChannel } = require('../models');
+const { ScrapeQ: { queue, tasks:
+   { cohort_scrape } } } = require('../queues/index');
+const scraper = require('slackmetascraper');
 
+queue.process(cohort_scrape, async ({ data: cohort_id }) => {
+  const wizard = await Wizard.findOne({ where: { cohort_id } });
 
-queue.process(cohort_scrape, ({ data }, done) => {
-  console.log(data);
+  // flesh this error out later
+  if (!wizard) return console.error('no wiz');
+
+  const channels = await CohortChannel.findAll({ where: { cohort_id } });
+  const slack_channel_ids = channels.map(
+    ({ slack_channel_id, last_slack_scrape_ts }) => ({
+      slack_channel_id,
+      last_slack_scrape_ts,
+    }),
+  );
+  const { slack_team_token } = wizard;
+
+  const scrape_results = await Promise.all(
+    slack_channel_ids.map(
+      ({ slack_channel_id, last_slack_scrape_ts }) => scraper(
+        slack_channel_id,
+        slack_team_token,
+        last_slack_scrape_ts,
+      ),
+    ),
+  );
+
+  return console.log(scrape_results);
 });
-
-// slack_team_token, slack_team_id, slack_channel_id, [start / end / count]
