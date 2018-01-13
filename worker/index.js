4ebe9f650193cfacddc7cfacbfd97d46e42bd112
The-Vampiire <vampiirecodes@gmail.com>
2018-01-12 21:25:59 -0500

    update cohort channel timestamp after scrape
    added 'dev-worker' script for local testing of the worker
    
    added update functionality to the cohort_channel after a succesful scrape
    
    tested and verified to be working
    
    go fuck yourself package lock

--- worker/index.js
+++ worker/index.js
@@ -3,34 +3,33 @@ console.log('Worker running - loggy boi');
 // DO NOT TOUCH - PRODUCTION READY
 // WE DONT KNOW HOW HE WORKS BUT HE DOES
 const { Wizard, CohortChannel } = require('../models');
-const { ScrapeQ: { queue, tasks:
-   { cohort_scrape } } } = require('../queues/index');
+const { ScrapeQ: { queue, tasks: { cohort_scrape } } } = require('../queues/index');
 const scraper = require('slackmetascraper');
 
-queue.process(cohort_scrape, async ({ data: cohort_id }) => {
+queue.process(cohort_scrape, async ({ data: cohort_id }, done) => {
   const wizard = await Wizard.findOne({ where: { cohort_id } });
 
   // flesh this error out later
   if (!wizard) return console.error('no wiz');
 
   const channels = await CohortChannel.findAll({ where: { cohort_id } });
-  const slack_channel_ids = channels.map(
-    ({ slack_channel_id, last_slack_scrape_ts }) => ({
-      slack_channel_id,
-      last_slack_scrape_ts,
-    }),
-  );
   const { slack_team_token } = wizard;
 
   const scrape_results = await Promise.all(
-    slack_channel_ids.map(
-      ({ slack_channel_id, last_slack_scrape_ts }) => scraper(
-        slack_channel_id,
-        slack_team_token,
-        last_slack_scrape_ts,
-      ),
-    ),
+    channels.map(channel => scraper(
+      channel.slack_channel_id,
+      slack_team_token,
+      channel.last_slack_scrape_ts,
+    ).then(async (channel_metadata) => {
+      if (channel_metadata.user_metadata) {
+        await channel.update({ last_slack_scrape_ts: channel_metadata.timestamp });
+        // TODO: update metadata table
+      }
+      return channel_metadata;
+    })),
   );
+
+  done();
 
   return console.log(scrape_results);
 });
