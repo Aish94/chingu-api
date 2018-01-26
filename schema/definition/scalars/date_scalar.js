baca4392c0f5cf5b1df25c887953430246b37062
The-Vampiire <vampiirecodes@gmail.com>
2018-01-25 22:33:00 -0500

    updated cohort_user_standups table
    - added `standup_type` enum and `standups` JSONB columns
    
    - created migration
    
    - added `standup_type` enum to typedefs
    
    - updated the cohort_user_standups typedef
    
    tested and passed migration

--- schema/definition/scalars/date_scalar.js
+++ schema/definition/scalars/date_scalar.js
@@ -1,7 +1,10 @@
 let DateScalar;
 
 module.exports = () => [DateScalar];
-
+/**
+ * TODO: fix this scalar currently returning the following error
+ * Error: Expected a value of type "Date" but received: Wed Dec 13 2017 03:59:24 GMT-0500 (EST)
+ */
 DateScalar = `
   scalar Date
 `;
